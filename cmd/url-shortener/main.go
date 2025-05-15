437ee557f6ec53a77f874fd58d1cd0d826d9c2e2
Lacky1234union <rogachevegorushka@yandex.ru>
2025-05-15 20:01:50 +0300

    save url and handler and ...

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Lacky1234union/UrlShorter/internal/config"
 	"github.com/Lacky1234union/UrlShorter/internal/lib/logger/sl"
 	"github.com/Lacky1234union/UrlShorter/internal/storage/sqlite"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 const (
@@ -27,6 +29,12 @@ func main() {
 	if err != nil {
 		log.Error("failed to initialize storage", sl.Err(err))
 	}
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID) // Добавляет request_id в каждый запрос, для трейсинга
+	router.Use(middleware.Logger)    // Логирование всех запросов
+	router.Use(middleware.Recoverer) // Если где-то внутри сервера (обработчика запроса) произойдет паника, приложение не должно упасть
+	router.Use(middleware.URLFormat) // Парсер URLов поступающих запросов
 }
 
 func setupLogger(env string) *slog.Logger {
---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,104 @@
+// internal/http-server/handlers/url/save/save.go
+package save
+
+import (
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/Lacky1234union/UrlShorter/internal/lib/api/response"
+	"github.com/Lacky1234union/UrlShorter/internal/lib/errs"
+	"github.com/Lacky1234union/UrlShorter/internal/lib/logger/sl"
+	"github.com/Lacky1234union/UrlShorter/internal/lib/random"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+type Response struct {
+	response.Response
+	Alias string `json:"alias,omitempty"`
+}
+type URLSaver interface {
+	SaveURL(URL, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO: move to config when needed
+		const aliasLength = 6
+		const op = "handlers.url.save.New"
+
+		// Добавляем к текущму объекту логгера поля op и request_id
+		// Они могут очень упростить нам жизнь в будущем
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		// Создаем объект запроса и анмаршаллим в него запрос
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if errors.Is(err, io.EOF) {
+			// Такую ошибку встретим, если получили запрос с пустым телом
+			// Обработаем её отдельно
+			log.Error("request body is empty")
+
+			render.JSON(w, r, response.Error("empty request")) // <----
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, response.Error("failed to decode request")) // <----
+
+			return
+		}
+
+		// Лучше больше логов, чем меньше - лишнее мы легко сможем почистить,
+		// при необходимости. А вот недостающую информацию мы уже не получим.
+		log.Info("request body decoded", slog.Any("req", req))
+
+		// ...
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, errs.ErrURLExists) {
+
+			// Отдельно обрабатываем ситуацию,
+			// когда запись с таким Alias уже существует
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, response.Error("url already exists"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to add url", sl.Err(err))
+
+			render.JSON(w, r, response.Error("failed to add url"))
+
+			return
+		}
+
+		log.Info("url added", slog.Int64("id", id))
+
+		responseOK(w, r, alias)
+	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, Response{
+		Response: response.OK(),
+		Alias:    alias,
+	})
+}
