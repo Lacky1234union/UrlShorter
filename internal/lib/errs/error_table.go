b8914d6628e9eccaef0fff2f3067d25b34812807
Lacky1234union <rogachevegorushka@yandex.ru>
2025-06-04 20:13:58 +0300

    tutor

--- internal/lib/errs/error_table.go
+++
@@ -1,8 +0,0 @@
-package errs
-
-import "errors"
-
-var (
-	ErrURLNotFound = errors.New("url not found")
-	ErrURLExists   = errors.New("url exists")
-)
--- internal/storage/storage.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-// URLStorage defines the interface for URL storage operations
-type URLStorage interface {
-	SaveURL(url string, alias string) (int64, error)
-	GetURL(alias string) (string, error)
-	DeleteURL(alias string) error
-}
