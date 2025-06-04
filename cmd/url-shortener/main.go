ba7f85d2496f8b747aaa64b4c770ff336186f4c4
Lacky1234union <rogachevegorushka@yandex.ru>
2025-06-04 20:54:43 +0300

    create setup logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/Lacky1234union/UrlShorter/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad() // TODO: init config: cleanenv
 
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+	log.Info("Server Start", slog.String("env", cfg.Env))
 	//
 	//TODO: init logger: slog (logger)
 	//
 	//TODO: init roouter: chi, "chi render"
 	//
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	case envProd:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}))
+	}
+	return log
 }
